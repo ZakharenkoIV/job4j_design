a4a86fcf2cd51b4c3b1ce32915752f8243765d89
ZakharenkoIV <zaharenkoiv93@gmail.com>
2020-06-06 12:12:04 +0300

    1. Что такое итератор.[4951#287604]

---
+++ chapter_001_-_data_structures_and_algorithms/src/test/java/ru/job4j/it/ArrayItTest.java
@@ -0,0 +1,39 @@
+package ru.job4j.it;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+
+import org.junit.Test;
+
+import java.util.NoSuchElementException;
+
+public class ArrayItTest {
+
+    @Test
+    public void whenMultiCallHasNextThenTrue() {
+        ArrayIt it = new ArrayIt(
+                new int[]{1, 2, 3}
+        );
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.hasNext(), is(true));
+    }
+
+    @Test
+    public void whenReadSequence() {
+        ArrayIt it = new ArrayIt(
+                new int[]{1, 2, 3}
+        );
+        assertThat(it.next(), is(3));
+        assertThat(it.next(), is(2));
+        assertThat(it.next(), is(1));
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void whenNextFromEmpty() {
+        ArrayIt it = new ArrayIt(
+                new int[]{}
+        );
+        it.next();
+    }
+}
