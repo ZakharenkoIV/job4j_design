37ef17d8d5932e750cda647ba992266af6a8236e
ZakharenkoIV <zaharenkoiv93@gmail.com>
2020-06-10 15:09:08 +0300

    5.2.1. Реализовать SimpleArray<T>[156#287611]

---
+++ chapter_001_-_data_structures_and_algorithms/src/main/java/ru/job4j/generic/SimpleArray.java
@@ -0,0 +1,91 @@
+package ru.job4j.generic;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
+public class SimpleArray<T> implements Iterable<T> {
+    Object[] objects;
+    int index = 0;
+
+    public SimpleArray(int arraySize) {
+        this.objects = new Object[arraySize];
+    }
+
+    //add(T model) - добавляет указанный элемент (model) в первую свободную ячейку.
+    public void add(T value) {
+        this.objects[index++] = value;
+    }
+
+    //get(int index) - возвращает элемент, расположенный по указанному индексу.
+    public T get(int position) {
+        return (T) this.objects[position];
+    }
+
+    //set(int index, T model) - заменяет указанным элементом (model) элемент, находящийся по индексу index.
+    public void set(int index, T model) {
+        objects[index] = model;
+    }
+
+    //remove(int index) - удаляет элемент по указанному индексу,
+    // все находящиеся справа элементы при этом необходимо сдвинуть на единицу влево
+    // (в середине массива не должно быть пустых ячеек).
+    public void remove(int index) {
+        if (objects.length - 1 - index >= 0) {
+            System.arraycopy(objects, index + 1, objects, index, objects.length - 1 - index);
+            objects[objects.length - 1] = null;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "SimpleArray " + Arrays.toString(objects);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        SimpleArray<?> that = (SimpleArray<?>) o;
+        return index == that.index && Arrays.equals(objects, that.objects);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(index);
+        result = 31 * result + Arrays.hashCode(objects);
+        return result;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        Object[] array = objects;
+        return new Iterator<>() {
+
+            private final Object[] objects = array;
+            private int position = 0;
+
+            @Override
+            public boolean hasNext() {
+                boolean result = false;
+                if (position < objects.length && objects[position] != null) {
+                    result = true;
+                }
+                return result;
+            }
+
+            @Override
+            public T next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                return (T) objects[position++];
+            }
+        };
+    }
+}
---
+++ chapter_001_-_data_structures_and_algorithms/src/test/java/ru/job4j/generic/SimpleArrayTest.java
@@ -0,0 +1,51 @@
+package ru.job4j.generic;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class SimpleArrayTest {
+    SimpleArray<String> simpleArray = new SimpleArray<>(3);
+
+    @Before
+    public void before() {
+        simpleArray.add("test1");
+        simpleArray.add("test2");
+        simpleArray.add("test3");
+    }
+
+    @Test
+    public void add() {
+
+        assertThat(simpleArray.toString(), is("SimpleArray [test1, test2, test3]"));
+    }
+
+    @Test
+    public void get() {
+        assertThat(simpleArray.get(1), is("test2"));
+    }
+
+    @Test
+    public void set() {
+        simpleArray.set(1, "test7");
+        assertThat(simpleArray.get(1), is("test7"));
+    }
+
+    @Test
+    public void remove() {
+        simpleArray.remove(1);
+        assertThat(simpleArray.toString(), is("SimpleArray [test1, test3, null]"));
+    }
+
+    @Test
+    public void iterator() {
+        SimpleArray<String> result = new SimpleArray<>(3);
+        for (String s : simpleArray) {
+            result.add(s);
+        }
+        simpleArray.iterator();
+        assertThat(result, is(simpleArray));
+    }
+}
