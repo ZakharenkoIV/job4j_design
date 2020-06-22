c561f5ab86771f09387dc27983c1de53a14fb744
ZakharenkoIV <zaharenkoiv93@gmail.com>
2020-06-22 13:41:11 +0300

    5. Очередь на двух стеках[160#287597]

--- chapter_001_-_data_structures_and_algorithms/src/main/java/ru/job4j/collection/SimpleQueue.java
+++ chapter_001_-_data_structures_and_algorithms/src/main/java/ru/job4j/collection/SimpleQueue.java
@@ -3,24 +3,24 @@ package ru.job4j.collection;
 public class SimpleQueue<T> {
     private final SimpleStack<T> in = new SimpleStack<>();
     private final SimpleStack<T> out = new SimpleStack<>();
-    private int counter = 0;
+    private int inCounter = 0;
+    private int outCounter = 0;
 
     public T poll() {
-        stackShift(out, in);
-        T result = out.pop();
-        counter--;
-        stackShift(in, out);
-        return result;
+        assert outCounter >= 0 : "outCounter < 0";
+        if (outCounter == 0) {
+            for (int i = 0; i < inCounter; i++) {
+                out.push(in.pop());
+            }
+            outCounter = inCounter;
+            inCounter = 0;
+        }
+        outCounter--;
+        return out.pop();
     }
 
     public void push(T value) {
         in.push(value);
-        counter++;
-    }
-
-    private void stackShift(SimpleStack<T> in, SimpleStack<T> out) {
-        for (int i = 0; i < counter; i++) {
-            in.push(out.pop());
-        }
+        inCounter++;
     }
 }
