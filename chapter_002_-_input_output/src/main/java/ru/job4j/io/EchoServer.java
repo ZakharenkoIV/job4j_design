b6ee85e1d5f8012695adbf5b01c50fb546bd8a3d
ZakharenkoIV <zaharenkoiv93@gmail.com>
2020-10-07 15:35:52 +0300

    1. Бот[7921#287657]

--- chapter_002_-_input_output/src/main/java/ru/job4j/io/EchoServer.java
+++ chapter_002_-_input_output/src/main/java/ru/job4j/io/EchoServer.java
@@ -17,14 +17,22 @@ public class EchoServer {
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
                     String str = in.readLine();
+                    String str1 = "";
                     while (!(str).isEmpty()) {
                         System.out.println(str);
-                        if (str.contains("=Bye")) {
-                            serverOperation = false;
+                        if (str.contains("GET")) {
+                            str1 = str;
                         }
                         str = in.readLine();
                     }
-                    out.write("HTTP/1.1 200 OK\r\n\\".getBytes());
+                    out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                    if (str1.contains("/?msg=Exit")) {
+                        serverOperation = false;
+                    } else if (str1.contains("/?msg=Hello")) {
+                        out.write("Hello\r\n\\".getBytes());
+                    } else {
+                        out.write(str1.substring(10, str1.lastIndexOf("HTTP/1.1")).getBytes());
+                    }
                 }
             }
         }
