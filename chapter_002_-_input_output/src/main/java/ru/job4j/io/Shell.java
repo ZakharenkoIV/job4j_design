02ae33d817a2077462a8f7b1b6ef6f59e351497b
ZakharenkoIV <zaharenkoiv93@gmail.com>
2020-11-21 15:20:04 +0300

    Доп. задание к блоку i/o

--- chapter_002_-_input_output/src/main/java/ru/job4j/io/Shell.java
+++ chapter_002_-_input_output/src/main/java/ru/job4j/io/Shell.java
@@ -1,26 +1,37 @@
 package ru.job4j.io;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Shell {
-    private String modPath = "";
+    private String catalogSeparator;
+    private String rootDirectory;
+    private List<String> listPath;
+
+    public Shell() {
+        this.catalogSeparator = "/";
+        this.rootDirectory = "/";
+        this.listPath = new LinkedList<>();
+    }
 
     public void cd(String path) {
-        if (path.startsWith("/")) {
-            modPath = path;
-        } else {
-            modPath = modPath.concat("/").concat(path);
-        }
-        if (modPath.endsWith("..")) {
-            int lastSlash = modPath.lastIndexOf("/");
-            int penultimateSlash = modPath.lastIndexOf("/", lastSlash - 1);
-            modPath = modPath.substring(0, penultimateSlash);
-
-        }
-        if (modPath.isEmpty()) {
-            modPath = "/";
+        listPath.addAll(Arrays.asList(path.split(catalogSeparator)));
+        if (listPath.size() != 0 && listPath.get(listPath.size() - 1).equals("..")) {
+            listPath.remove(listPath.size() - 1);
+            listPath.remove(listPath.size() - 1);
         }
     }
 
     public String pwd() {
-        return modPath;
+        String stringPath = "";
+        for (String s : listPath) {
+            stringPath = stringPath.concat(catalogSeparator).concat(s);
+        }
+        if (listPath.size() == 0) {
+            stringPath = rootDirectory;
+
+        }
+        return stringPath;
     }
 }
