2a6768d0f921604427df33aabf4999082476c907
ZakharenkoIV <zaharenkoiv93@gmail.com>
2020-11-22 15:53:13 +0300

    Поиск файлов по критерию[783#287653]

--- chapter_002_-_input_output/src/main/java/ru/job4j/io/Find.java
+++ chapter_002_-_input_output/src/main/java/ru/job4j/io/Find.java
@@ -1,29 +1,46 @@
 package ru.job4j.io;
 
-import java.io.*;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Predicate;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Find {
     private ArgFind argFind;
-
+    private Map<String, Predicate<Path>> keySearchExpression = new HashMap<>();
     private List<Path> paths = new ArrayList<>();
 
     public Find(ArgFind argFind) {
         this.argFind = argFind;
+        loadKeySearchExpression();
     }
 
-    public void toFind() {
-        findAllPathsByName();
-        saveToFile();
+    private void loadKeySearchExpression() {
+        keySearchExpression.put("-m", p -> p.toFile().getName().endsWith(argFind.required()));
+        keySearchExpression.put("-f", p -> {
+            String pathFilename = p.toFile().getName();
+            String fileName = pathFilename.substring(pathFilename.lastIndexOf("/") + 1);
+            return fileName.equals(argFind.required());
+        });
+        keySearchExpression.put("-r", p -> {
+            Pattern pattern = Pattern.compile("\\\\" + argFind.required());
+            String pathFilename = p.toFile().getName();
+            String fileName = pathFilename.substring(pathFilename.lastIndexOf("/") + 1);
+            Matcher m = pattern.matcher(fileName);
+            return m.find();
+        });
     }
 
     private void findAllPathsByName() {
         try {
-            SearchFiles searchFiles = new SearchFiles(
-                    p -> p.toFile().getName().endsWith(argFind.required()));
+            SearchFiles searchFiles = new SearchFiles(keySearchExpression.get(argFind.searchBy()));
             Files.walkFileTree(argFind.directory(), searchFiles);
             paths = searchFiles.getPaths();
         } catch (IOException e) {
@@ -32,8 +49,7 @@ public class Find {
     }
 
     private void saveToFile() {
-        try {
-            FileWriter out = new FileWriter(argFind.output(), false);
+        try (FileWriter out = new FileWriter(argFind.output(), false)) {
             for (Path path : paths) {
                 out.write(path.toString() + System.lineSeparator());
             }
@@ -41,6 +57,11 @@ public class Find {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public void toFind() {
+        findAllPathsByName();
+        saveToFile();
     }
 
     public static void main(String[] args) {
