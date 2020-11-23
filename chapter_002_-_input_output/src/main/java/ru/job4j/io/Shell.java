49ff0a05f783751b9fd131deb2fc375e4ce5619c
ZakharenkoIV <zaharenkoiv93@gmail.com>
2020-11-23 13:05:07 +0300

    Вопросы[571#287652]

--- chapter_002_-_input_output/src/main/java/ru/job4j/io/Shell.java
+++ chapter_002_-_input_output/src/main/java/ru/job4j/io/Shell.java
@@ -2,35 +2,32 @@ package ru.job4j.io;
 
 import java.util.Arrays;
 import java.util.LinkedList;
-import java.util.List;
 
 public class Shell {
     private String catalogSeparator;
     private String rootDirectory;
-    private List<String> listPath;
+    private LinkedList<String> listPath = new LinkedList<>();
 
     public Shell() {
         this.catalogSeparator = "/";
         this.rootDirectory = "/";
-        this.listPath = new LinkedList<>();
     }
 
     public void cd(String path) {
         listPath.addAll(Arrays.asList(path.split(catalogSeparator)));
-        if (listPath.size() != 0 && listPath.get(listPath.size() - 1).equals("..")) {
-            listPath.remove(listPath.size() - 1);
-            listPath.remove(listPath.size() - 1);
+        if (!listPath.isEmpty() && listPath.peekLast().equals("..")) {
+            listPath.pollLast();
+            listPath.pollLast();
         }
     }
 
     public String pwd() {
-        String stringPath = "";
-        for (String s : listPath) {
-            stringPath = stringPath.concat(catalogSeparator).concat(s);
-        }
-        if (listPath.size() == 0) {
-            stringPath = rootDirectory;
-
+        String stringPath = rootDirectory;
+        if (!listPath.isEmpty()) {
+            for (String s : listPath) {
+                stringPath = stringPath.concat(s).concat(catalogSeparator);
+            }
+            stringPath = stringPath.substring(0, stringPath.length() - 1);
         }
         return stringPath;
     }
