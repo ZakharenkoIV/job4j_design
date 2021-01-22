ca1d140bb8088bee5b0476e0786b54c98b44e8ba
ZakharenkoIV <zaharenkoiv93@gmail.com>
2021-01-22 20:56:33 +0300

    Экзамен. Sql[1730#287584]

---
+++ chapter_003_-_database/src/main/java/ru/job4j/database/sqlExamination.sql
@@ -0,0 +1,35 @@
+CREATE DATABASE examination;
+
+CREATE TABLE meetings
+(
+    id   serial primary key,
+    name varchar(250) not null
+);
+
+CREATE TABLE users
+(
+    id   serial primary key,
+    name varchar(250) not null
+);
+
+CREATE TABLE usersMeetings
+(
+    userId             int          not null,
+    meetingId          int          not null,
+    appointment_status varchar(250) not null
+);
+
+SELECT all_meeting, quantity_users
+from (select count(*) from usersMeetings) as all_meeting
+         cross join (select count(appointment_status)
+                     from usersMeetings
+                     where appointment_status LIKE 'принял') as quantity_users;
+
+select name
+from meetings
+         right join (select meetingId
+                     from usersMeetings
+                     group by meetingId
+                     having count(appointment_status) filter ( where appointment_status like 'принял') = 0) m
+                    on meetings.id = m.meetingId
+;
